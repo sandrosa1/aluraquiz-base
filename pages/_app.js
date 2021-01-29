9adcd126e45c75dfd1706e36729d820d4aee9900
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-01-29 00:15:27 -0300

    Segunda aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json'
+import Head from 'next/head'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -11,7 +12,8 @@ const GlobalStyle = createGlobalStyle`
     /* New styles */
     display: flex;
     flex-direction: column;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Averia Serif Libre', cursive;
+    font-weight:700;
     // Deixa branco no começo
     color: ${({ theme }) => theme.colors.contrastText};
   }
@@ -31,6 +33,23 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <Head>
+          <title>Copas Quiz</title>
+          <meta name="title" content="Copas Quiz"/>
+          <meta name="description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
+          <meta property="og:title" content="Copas Quiz"/>
+          <meta property="og:description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
+          <meta property="og:image" content=""/>
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
+          <meta property="twitter:title" content="Copas Quiz"/>
+          <meta property="twitter:description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
+          <meta property="twitter:image" content=""/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
+        </Head>
       <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
+import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-import  QuizLogo from '../src/components/QuizLogo'
-import  QuizBackground from '../src/components/QuizBackground'
-
-
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
 
 
 const Title = styled.h1`
@@ -35,27 +35,49 @@ export const QuizContainer = styled.div`
 `;
 
 
-
-
 export default function Home() {
-    return (
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState',name, setName);
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Copas Quiz</title>
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
         <Widget>
+          <Widget.Header> <h1>Copas Quiz</h1></Widget.Header>
           <Widget.Content>
-          <h1>Copas Quiz</h1>
-          <p>Teste seus conhecimentos</p>
-          </Widget.Content> 
+            <form onSubmit={function (infosDoEvento){
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Submit');
+              }}
+            >
+              <input 
+                onChange={function (infosDoEvento)  {
+                //name = infosDoEvento.target.value;
+                setName(infosDoEvento.target.value)
+              }}
+               placeholder="Manda seu nome"/>
+              <button type="submit" disabled={name.length === 0}>
+                Jogar 
+                {name}
+                </button>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
-        <Widget.Content>
-          <h1>Copas Quiz</h1>
-        <p>Teste seus conhecimentos</p>
-        </Widget.Content>
+          <Widget.Content>
+            <h1>Copas Quiz</h1>
+            <p>Teste seus conhecimentos</p>
+          </Widget.Content>
         </Widget>
         <Footer></Footer>
         <GitHubCorner projectUrl="https://github.com/sandrosa1"></GitHubCorner>
-        </QuizContainer >
-      </QuizBackground>
-    );
-  }
+      </QuizContainer >
+    </QuizBackground>
+  );
+}
