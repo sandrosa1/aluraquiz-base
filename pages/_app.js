ad88e090f7d3e55c7db013a3ed14caabe63b8e7b
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-01-31 10:05:09 -0300

    Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -38,18 +38,18 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Head>
           <title>Copas Quiz</title>
-          <meta name="title" content="Copas Quiz"/>
-          <meta name="description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
+          <meta name="title" content="Historias das Copas Quiz"/>
+          <meta name="description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
           <meta property="og:title" content="Copas Quiz"/>
-          <meta property="og:description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
-          <meta property="og:image" content=""/>
+          <meta property="og:description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
+          <meta property="og:image" content={bg}/>
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
           <meta property="twitter:title" content="Copas Quiz"/>
-          <meta property="twitter:description" content="Um super quiz para testar seu conhecimento e copas do mundo."/>
-          <meta property="twitter:image" content=""/>
+          <meta property="twitter:description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
+          <meta property="twitter:image" content={bg}/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
         </Head>
