521430840fe4f69a5b0e8d8b583b04ec8bc1a0e6
sandrosa1 <sandro.sa@fatec.sp.gov.br>
2021-01-31 14:23:31 -0300

    meta tags favicon]

--- pages/_app.js
+++ pages/_app.js
@@ -39,17 +39,18 @@ export default function App({ Component, pageProps }) {
         <Head>
           <title>Copas Quiz</title>
           <meta name="title" content="Historias das Copas Quiz"/>
-          <meta name="description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
+          <meta name="description" content="Um super quiz para testar seus conhecimentos em Copas do Mundo."/>
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
           <meta property="og:title" content="Copas Quiz"/>
-          <meta property="og:description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
-          <meta property="og:image" content={bg}/>
+          <meta property="og:description" content="Um super quiz para testar seus conhecimentos em Copas do Mundo."/>
+          <meta property="og:image" content="{bg}"/>
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://historias-das-copas-git-main.sandrosa1.vercel.app/"/>
           <meta property="twitter:title" content="Copas Quiz"/>
-          <meta property="twitter:description" content="Um super quiz para testar seus conhecimentos em copas do mundo."/>
-          <meta property="twitter:image" content={bg}/>
+          <meta property="twitter:description" content="Um super quiz para testar seus conhecimentos em Copas do Mundo."/>
+          <meta property="twitter:image" content="/chuteInicial1.jpg"/>
+          <link rel="shortcut icon" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Averia+Serif+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
         </Head>
